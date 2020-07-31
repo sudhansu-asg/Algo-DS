3b737452d853775fcf733c816578723559cdc26d
Sudhansu.Badajena <Sudhansu.Badajena@asg.com>
2020-07-31 13:18:05 +0530

    My First Commit

---
+++ DataStructures/src/com/sudhansu/practice/Arrays/MyFirstCheckin.java
@@ -0,0 +1,7 @@
+package com.sudhansu.practice.Arrays;
+
+public class MyFirstCheckin {
+    public static void main(String[] args) {
+        System.out.println("This is my First check in :");
+    }
+}
